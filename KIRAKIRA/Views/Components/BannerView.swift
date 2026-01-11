fd78cb64a9c78b6692df8b03edb8dbafecd6bcf5
Aira Sakuranomiya <aira@aira.cafe>
2026-01-11 17:10:57 +0800

    Add banner to edit profile page

---
+++ KIRAKIRA/Views/Components/BannerView.swift
@@ -0,0 +1,47 @@
+//
+//  BannerView.swift
+//  KIRAKIRA
+//
+//  Created by Aira Sakuranomiya on 2026/01/11.
+//
+
+import SwiftUI
+import VariableBlur
+
+struct BannerView: View {
+    var body: some View {
+        VStack {
+            Image("DefaultBanner")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(
+                    minWidth: 0,
+                    minHeight: 200,
+                    maxHeight: 200
+                )
+                .aspectRatio(contentMode: .fit)
+                .clipped()
+                .overlay(alignment: .bottom) {
+                    VariableBlurView(
+                        maxBlurRadius: 10,
+                        direction: .blurredBottomClearTop
+                    ).frame(height: 50)
+                }
+                .mask(
+                    LinearGradient(
+                        gradient: Gradient(colors: [
+                            Color.black,
+                            Color.black.opacity(0),
+                        ]),
+                        startPoint: .center,
+                        endPoint: .bottom
+                    )
+                )
+                .backgroundExtensionEffect()
+        }
+    }
+}
+
+#Preview {
+    BannerView()
+}
