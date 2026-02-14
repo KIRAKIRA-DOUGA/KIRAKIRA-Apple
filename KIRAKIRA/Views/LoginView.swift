af2bfe02734209dc794a85d474c60201dc130b6d
Yasaitori <caijidayo@outlook.com>
2026-02-14 14:38:44 +0800

    feat： add verify view

--- KIRAKIRA/Views/LoginView.swift
+++ KIRAKIRA/Views/LoginView.swift
@@ -1,9 +1,52 @@
 import SwiftUI
 
+struct EmailVerifcationView: View {
+    @State private var verificationCode = ""
+
+    var body: some View {
+        WizardForm(image: "Logo", title: .verifyYourIdentity, subtitle: .verifyEmailDescription) {
+            WizardSection {
+                TextField(.verificationCode, text: $verificationCode)
+                    .textContentType(.oneTimeCode)
+                    .disableAutocorrection(true)
+                    .textInputAutocapitalization(.never)
+                    .keyboardType(.numberPad)
+            }
+            Button(.resend) {
+
+            }.buttonStyle(.borderless)
+        } footer: {
+            Button(.linkStart) {
+
+            }
+        }
+    }
+}
+
+struct TotpVerifcationView: View {
+    @State private var verificationCode = ""
+
+    var body: some View {
+        WizardForm(image: "Logo", title: .verifyYourIdentity, subtitle: .verifyTotpDescription) {
+            WizardSection {
+                TextField(.verificationCode, text: $verificationCode)
+                    .textContentType(.oneTimeCode)
+                    .disableAutocorrection(true)
+                    .textInputAutocapitalization(.never)
+                    .keyboardType(.numbersAndPunctuation)
+            }
+        } footer: {
+            Button(.linkStart) {
+
+            }
+        }
+    }
+}
+
 struct LoginView: View {
     @State private var emailAddress = ""
     @State private var password = ""
-    
+
     var body: some View {
         WizardForm(image: "Logo", title: .logIn, subtitle: .logInDescription) {
             WizardSection {
@@ -12,22 +55,22 @@ struct LoginView: View {
                     .disableAutocorrection(true)
                     .textInputAutocapitalization(.never)
                     .keyboardType(.emailAddress)
-                
+
                 SecureField(.password, text: $password)
                     .textContentType(.password)
             }
-            
+
             Button(.createAccount, systemImage: "plus.circle") {
-                
+
             }.buttonStyle(.borderless)
         } footer: {
             Button(.linkStart) {
-                
+
             }
         }
     }
 }
 
 #Preview {
-    LoginView()
+    EmailVerifcationView()
 }
