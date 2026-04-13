89e3ac271b086c1a453f347e28ffc063d5bcc3f8
Aira Sakuranomiya <aira@aira.cafe>
2026-04-13 08:44:20 +0800

    Use UIActivityIndicatorView for LoadingView

--- KIRAKIRA/Components/States/LoadingView.swift
+++ KIRAKIRA/Components/States/LoadingView.swift
@@ -1,5 +1,17 @@
+// 细致的你一定发现了「ProgressView.controlSize(.large)」和「UIActivityIndicatorView(style: .large)」看上去不一样，SwiftUI 每一瓣都比 UIKit 的大了一点，造成中心很小。因此我们不使用 ProgressView。
+
 import SwiftUI
-import SwiftUIIntrospect
+
+struct ActivityIndicator: UIViewRepresentable {
+    func makeUIView(context: Context) -> UIActivityIndicatorView {
+        let view = UIActivityIndicatorView(style: .large)
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIActivityIndicatorView, context: Context) {
+        uiView.startAnimating()
+    }
+}
 
 struct LoadingView: View {
     @State private var isShowingProgress: Bool = false
@@ -8,13 +20,7 @@ struct LoadingView: View {
         VStack {
             Spacer()
             if isShowingProgress {
-                ProgressView()
-                    .controlSize(.large)
-                /*
-                    .introspect(.progressView(style: .circular), on: .iOS(.v26)) { UIActivityIndicatorView in
-                        UIActivityIndicatorView.style = .large
-                    }  // 细致的你一定发现了设置controlSize为.large的和UIKit层的设置了style为.large的看上去不一样，UIKit每一瓣都比SwiftUI的小一像素。
-                 */
+                ActivityIndicator()
             }
             Spacer()
         }
