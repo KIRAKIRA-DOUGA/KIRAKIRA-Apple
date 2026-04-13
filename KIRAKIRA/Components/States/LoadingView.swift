57e86ba4762766d2d0296a2b2ce3c6a4fc473af4
Aira Sakuranomiya <aira@aira.cafe>
2026-04-13 08:48:28 +0800

    Update LoadingView.swift

--- KIRAKIRA/Components/States/LoadingView.swift
+++ KIRAKIRA/Components/States/LoadingView.swift
@@ -1,8 +1,10 @@
-// 细致的你一定发现了「ProgressView.controlSize(.large)」和「UIActivityIndicatorView(style: .large)」看上去不一样，SwiftUI 每一瓣都比 UIKit 的大了一点，造成中心很小。因此我们不使用 ProgressView。
+// 细致的你一定发现了「ProgressView.controlSize(.large)」和「UIActivityIndicatorView(style: .large)」看上去不一样。
+// SwiftUI 每一瓣都比 UIKit 的大了一点，造成中心很小。
+// 根据官方设计文档，它应该是长 UIKit 的样子才对，因此我们不使用 ProgressView。
 
 import SwiftUI
 
-struct ActivityIndicator: UIViewRepresentable {
+private struct ActivityIndicator: UIViewRepresentable {
     func makeUIView(context: Context) -> UIActivityIndicatorView {
         let view = UIActivityIndicatorView(style: .large)
         return view
